d4fc240652f7619b2d8ff69925af1c1a78324ef4
oskar <53711292+oskarkocol@users.noreply.github.com>
2026-03-24 17:31:29 +1100

    add codecov

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -3,7 +3,11 @@ import pkg from './package.json' with { type: 'json' };
 
 export default defineConfig({
   test: {
-    globals: true, 
+    globals: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+    },
   },
   define: {
     __PACKAGE_VERSION__: JSON.stringify(pkg.version),
