46baf8e959f06535c4ff7386555232160192fdc4
oskar <53711292+oskarkocol@users.noreply.github.com>
2026-03-26 17:23:35 +1100

    add utilities to the testing package

--- packages/testing/src/ai-sdk/index.ts
+++ packages/testing/src/ai-sdk/index.ts
@@ -1,3 +1,6 @@
 export * from './mock-language-model-v3.js';
 export * from './not-implemented.js';
 export * from './convert-array-to-streamable-text.js';
+export * from './consume-stream.js';
+export * from './convert-async-iterable-to-array.js';
+export * from './convert-readable-stream-to-array.js';
