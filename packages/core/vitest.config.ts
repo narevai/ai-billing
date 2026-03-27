28e58fcf8767f00c6051922dc0b069241c203c19
oskar <53711292+oskarkocol@users.noreply.github.com>
2026-03-27 15:39:31 +1100

    test(core): set up edge tests

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -2,15 +2,38 @@ import { defineConfig } from 'vitest/config';
 import pkg from './package.json' with { type: 'json' };
 
 export default defineConfig({
+  define: {
+    __PACKAGE_VERSION__: JSON.stringify(pkg.version),
+  },
+  resolve: {
+    tsconfigPaths: true,
+  },
   test: {
-    globals: true,
     coverage: {
       provider: 'v8',
       reporter: ['text', 'lcov'],
       exclude: ['packages/core/src/error/**', '**/*.test.ts'],
     },
-  },
-  define: {
-    __PACKAGE_VERSION__: JSON.stringify(pkg.version),
+    projects: [
+      {
+        test: {
+          name: 'node',
+          globals: true,
+          environment: 'node',
+          exclude: ['node_modules/**'],
+        },
+      },
+      {
+        test: {
+          name: 'edge',
+          globals: true,
+          environment: 'edge-runtime',
+          typecheck: {
+            enabled: true,
+            tsconfig: './tsconfig.edge.json',
+          },
+        },
+      },
+    ],
   },
 });
