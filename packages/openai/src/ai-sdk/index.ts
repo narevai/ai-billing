718bec6de30794fd27251f581e8ea6fd97cada26
oskar <53711292+oskarkocol@users.noreply.github.com>
2026-03-31 15:18:19 +1100

    fix exports

--- packages/openai/src/ai-sdk/index.ts
+++ packages/openai/src/ai-sdk/index.ts
@@ -1,2 +1,2 @@
-export { createOpenRouterV3Middleware } from './language-model-middleware/index.js';
-export { createOpenRouterV3Middleware as createOpenRouterMiddleware } from './language-model-middleware/index.js';
+export { createOpenAIV3Middleware } from './language-model-middleware/index.js';
+export { createOpenAIV3Middleware as createOpenRouterMiddleware } from './language-model-middleware/index.js';
