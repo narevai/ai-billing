cda63b6cb1e1a60aa13701a5dcaf9fb78c44d908
oskar <53711292+oskarkocol@users.noreply.github.com>
2026-05-03 15:00:33 +0700

    test: crank timeouts

--- examples/chatbot-with-billing/playwright.config.ts
+++ examples/chatbot-with-billing/playwright.config.ts
@@ -47,9 +47,9 @@ export default defineConfig({
   },
 
   /* Configure global timeout for each test */
-  timeout: 10 * 1000,
+  timeout: 30 * 1000,
   expect: {
-    timeout: 10 * 1000,
+    timeout: 30 * 1000,
   },
 
   /* Configure projects */
@@ -97,7 +97,7 @@ export default defineConfig({
   webServer: {
     command: 'pnpm dev',
     url: `${baseURL}/ping`,
-    timeout: 5 * 1000,
+    timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
 });
